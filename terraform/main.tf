9f542ef7a5ab857fe93cbf2385aa4fac15524b8b
Alonso Tagle <dev@alonsotagle.com>
2025-02-02 12:10:54 -0600

    Sandbox account

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,3 +25,31 @@ module "email" {
 
   account_customizations_name = "comms"
 }
+
+module "sandbox" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "alonsotaglecom+awssandbox@gmail.com"
+    AccountName               = "sandbox-aft"
+    ManagedOrganizationalUnit = "AFT"
+    SSOUserEmail              = "alonsotaglecom+awssandbox@gmail.com"
+    SSOUserFirstName          = "Sandbox"
+    SSOUserLastName           = "AFT"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
