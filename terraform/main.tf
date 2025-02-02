d57bfa98bbf0bb68df7e78a46900ab243f449538
Alonso Tagle <dev@alonsotagle.com>
2025-02-02 15:43:25 -0600

    Remove sandbox account

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,14 @@
+locals {
+  account_comms = "Comms (ou-2e4r-45ykgo1b)"
+}
+
 module "email" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
     AccountEmail              = "alonsotaglecom+awsemail@gmail.com"
     AccountName               = "Email"
-    ManagedOrganizationalUnit = "Comms"
+    ManagedOrganizationalUnit = local.account_comms
     SSOUserEmail              = "alonsotaglecom+awsemail@gmail.com"
     SSOUserFirstName          = "Alonso"
     SSOUserLastName           = "Tagle"
@@ -24,34 +28,6 @@ module "email" {
   }
 
   account_customizations_name = "comms"
-}
-
-module "sandbox" {
-  source = "./modules/aft-account-request"
-
-  control_tower_parameters = {
-    AccountEmail              = "alonsotaglecom+awssandbox@gmail.com"
-    AccountName               = "sandbox-aft"
-    ManagedOrganizationalUnit = "AFT"
-    SSOUserEmail              = "alonsotaglecom+awssandbox@gmail.com"
-    SSOUserFirstName          = "Sandbox"
-    SSOUserLastName           = "AFT"
-  }
-
-  account_tags = {
-    "Learn Tutorial" = "AFT"
-  }
-
-  change_management_parameters = {
-    change_requested_by = "HashiCorp Learn"
-    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
-  }
-
-  custom_fields = {
-    group = "non-prod"
-  }
-
-  account_customizations_name = "sandbox"
 }
 
 module "sandbox_account_01" {
