eaf576ba8d86f2acb82d74655c574e8640e7f17c
Alonso Tagle <dev@alonsotagle.com>
2025-02-02 18:02:29 -0600

    Remove Email account

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,34 +2,6 @@ locals {
   account_comms = "Comms (ou-2e4r-45ykgo1b)"
 }
 
-module "email" {
-  source = "./modules/aft-account-request"
-
-  control_tower_parameters = {
-    AccountEmail              = "alonsotaglecom+awsemail@gmail.com"
-    AccountName               = "Email"
-    ManagedOrganizationalUnit = "Comms"
-    SSOUserEmail              = "alonsotaglecom+awsemail@gmail.com"
-    SSOUserFirstName          = "Alonso"
-    SSOUserLastName           = "Tagle"
-  }
-
-  account_tags = {
-    "ManagedBy" = "Terraform - AFT"
-  }
-
-  change_management_parameters = {
-    change_requested_by = "Alonso Tagle"
-    change_reason       = "Creating Email account"
-  }
-
-  custom_fields = {
-    group = "non-prod"
-  }
-
-  account_customizations_name = "comms"
-}
-
 module "comms_email" {
   source = "./modules/aft-account-request"
 
