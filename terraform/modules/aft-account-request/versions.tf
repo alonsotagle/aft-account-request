b5743ee796d7d4ef0d332fbde8be24841befd03e
Alonso Tagle <dev@alonsotagle.com>
2025-02-10 14:02:50 -0600

    Remove tfe workspace

--- terraform/modules/aft-account-request/versions.tf
+++ terraform/modules/aft-account-request/versions.tf
@@ -8,11 +8,5 @@ terraform {
       source  = "integrations/github"
       version = "~> 6.0"
     }
-    tfe = {
-      source  = "hashicorp/tfe"
-      version = "~> 0.63"
-    }
   }
 }
-
-data "aws_caller_identity" "current" {}
