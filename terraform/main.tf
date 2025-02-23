109b5bcceb6ffa5cd04296d8ab55b8c7fd71e562
Alonso Tagle <dev@alonsotagle.com>
2025-02-23 15:53:17 -0600

    feature: Add Finance/expenses account

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,6 @@
 locals {
-  account_comms = "Comms (ou-2e4r-45ykgo1b)"
+  ou_comms   = "Comms (ou-2e4r-45ykgo1b)"
+  ou_finance = "Finance (ou-2e4r-vvvnvvei)"
 }
 
 module "comms_email" {
@@ -8,7 +9,7 @@ module "comms_email" {
   control_tower_parameters = {
     AccountEmail              = "alonsotaglecom+email@gmail.com"
     AccountName               = "email"
-    ManagedOrganizationalUnit = local.account_comms
+    ManagedOrganizationalUnit = local.ou_comms
     SSOUserEmail              = "alonsotaglecom+email@gmail.com"
     SSOUserFirstName          = "Alonso"
     SSOUserLastName           = "Tagle"
@@ -29,4 +30,33 @@ module "comms_email" {
   }
 
   account_customizations_name = "comms"
+}
+
+module "finance_expenses" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "alonsotaglecom+expenses@gmail.com"
+    AccountName               = "expenses"
+    ManagedOrganizationalUnit = local.ou_finance
+    SSOUserEmail              = "alonsotaglecom+expenses@gmail.com"
+    SSOUserFirstName          = "Alonso"
+    SSOUserLastName           = "Tagle"
+  }
+
+  aft_account_ref = "aft-finance-expenses" # Name of the Github repo and Terraform Workspace
+
+  account_tags = {
+    "ManagedBy" = "Terraform - AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Alonso Tagle"
+    change_reason       = "Creating Finance/expenses account"
+  }
+
+  custom_fields = {
+  }
+
+  account_customizations_name = "finance"
 }
