b5fd69bcb6a6e311753703f7c61162d8d4cb3b60
Alonso Tagle <dev@alonsotagle.com>
2025-03-27 20:25:26 -0600

    fix: remove finance account

--- terraform/main.tf
+++ terraform/main.tf
@@ -61,31 +61,31 @@ module "comms_maia" {
   account_customizations_name = "comms"
 }
 
-module "finance_expenses" {
-  source = "./modules/aft-account-request"
+# module "finance_expenses" {
+#   source = "./modules/aft-account-request"
 
-  control_tower_parameters = {
-    AccountEmail              = "alonsotaglecom+expenses@gmail.com"
-    AccountName               = "expenses"
-    ManagedOrganizationalUnit = local.ou_finance
-    SSOUserEmail              = "alonsotaglecom+expenses@gmail.com"
-    SSOUserFirstName          = "Alonso"
-    SSOUserLastName           = "Tagle"
-  }
+#   control_tower_parameters = {
+#     AccountEmail              = "alonsotaglecom+expenses@gmail.com"
+#     AccountName               = "expenses"
+#     ManagedOrganizationalUnit = local.ou_finance
+#     SSOUserEmail              = "alonsotaglecom+expenses@gmail.com"
+#     SSOUserFirstName          = "Alonso"
+#     SSOUserLastName           = "Tagle"
+#   }
 
-  aft_account_ref = "aft-finance-expenses" # Name of the Github repo and Terraform Workspace
+#   aft_account_ref = "aft-finance-expenses" # Name of the Github repo and Terraform Workspace
 
-  account_tags = {
-    "ManagedBy" = "Terraform - AFT"
-  }
+#   account_tags = {
+#     "ManagedBy" = "Terraform - AFT"
+#   }
 
-  change_management_parameters = {
-    change_requested_by = "Alonso Tagle"
-    change_reason       = "Creating Finance/expenses account"
-  }
+#   change_management_parameters = {
+#     change_requested_by = "Alonso Tagle"
+#     change_reason       = "Creating Finance/expenses account"
+#   }
 
-  custom_fields = {
-  }
+#   custom_fields = {
+#   }
 
-  account_customizations_name = "finance"
-}
+#   account_customizations_name = "finance"
+# }
