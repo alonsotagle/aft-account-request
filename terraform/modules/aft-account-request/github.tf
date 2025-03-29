de51c5dd6337609c4983cf6d14c91348732c65f3
Alonso Tagle <dev@alonsotagle.com>
2025-03-29 14:40:55 -0600

    remove inner github provider

--- terraform/modules/aft-account-request/github.tf
+++ terraform/modules/aft-account-request/github.tf
@@ -1,7 +1,7 @@
-provider "github" {
-  owner = lookup(var.tfe_parameters, "gh_owner")
-  token = lookup(var.tfe_parameters, "github_token")
-}
+# provider "github" {
+#   owner = lookup(var.tfe_parameters, "gh_owner")
+#   token = lookup(var.tfe_parameters, "github_token")
+# }
 
 # resource "github_repository" "repo" {
 #   name        = var.aft_account_ref
