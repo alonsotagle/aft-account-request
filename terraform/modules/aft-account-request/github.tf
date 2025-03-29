3d188038ae50d551d89e85b32375682dd4d4f31e
Alonso Tagle <dev@alonsotagle.com>
2025-03-29 14:19:19 -0600

    fix: remove gh repos

--- terraform/modules/aft-account-request/github.tf
+++ terraform/modules/aft-account-request/github.tf
@@ -3,20 +3,20 @@ provider "github" {
   token = lookup(var.tfe_parameters, "github_token")
 }
 
-resource "github_repository" "repo" {
-  name        = var.aft_account_ref
-  description = "This repository is created automatically by AWS AFT account creation from a template."
+# resource "github_repository" "repo" {
+#   name        = var.aft_account_ref
+#   description = "This repository is created automatically by AWS AFT account creation from a template."
 
-  visibility             = "private"
-  allow_merge_commit     = false
-  allow_squash_merge     = true
-  allow_rebase_merge     = true
-  delete_branch_on_merge = true
-  vulnerability_alerts   = true
+#   visibility             = "private"
+#   allow_merge_commit     = false
+#   allow_squash_merge     = true
+#   allow_rebase_merge     = true
+#   delete_branch_on_merge = true
+#   vulnerability_alerts   = true
 
-  template {
-    owner                = "alonsotagle"
-    repository           = "template-aws-aft"
-    include_all_branches = false
-  }
-}
+#   template {
+#     owner                = "alonsotagle"
+#     repository           = "template-aws-aft"
+#     include_all_branches = false
+#   }
+# }
