1d589e00f1bf74af7c51af84562bfc5229317e49
Alonso Tagle <dev@alonsotagle.com>
2025-03-29 14:54:14 -0600

    return gh repo

--- terraform/modules/aft-account-request/github.tf
+++ terraform/modules/aft-account-request/github.tf
@@ -1,22 +1,17 @@
-# provider "github" {
-#   owner = lookup(var.tfe_parameters, "gh_owner")
-#   token = lookup(var.tfe_parameters, "github_token")
-# }
+resource "github_repository" "repo" {
+  name        = var.aft_account_ref
+  description = "This repository is created automatically by AWS AFT account creation from a template."
 
-# resource "github_repository" "repo" {
-#   name        = var.aft_account_ref
-#   description = "This repository is created automatically by AWS AFT account creation from a template."
+  visibility             = "private"
+  allow_merge_commit     = false
+  allow_squash_merge     = true
+  allow_rebase_merge     = true
+  delete_branch_on_merge = true
+  vulnerability_alerts   = true
 
-#   visibility             = "private"
-#   allow_merge_commit     = false
-#   allow_squash_merge     = true
-#   allow_rebase_merge     = true
-#   delete_branch_on_merge = true
-#   vulnerability_alerts   = true
-
-#   template {
-#     owner                = "alonsotagle"
-#     repository           = "template-aws-aft"
-#     include_all_branches = false
-#   }
-# }
+  template {
+    owner                = "alonsotagle"
+    repository           = "template-aws-aft"
+    include_all_branches = false
+  }
+}
